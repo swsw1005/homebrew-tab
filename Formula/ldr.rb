4e4d7f0bb5aa371d784e010d163de6510a7e1b0c
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2026-09-14 00:22:34 +0900

    fix url line

--- Formula/ldr.rb
+++ Formula/ldr.rb
@@ -5,14 +5,12 @@ class Ldr < Formula
 
   on_macos do
     on_arm do
-      url
-      "https://github.com/swsw1005/local-dev-launcher/releases/download/v#{version}/ldr_#{version}_darwin_arm64.tar.gz"
+      url "https://github.com/swsw1005/local-dev-launcher/releases/download/v#{version}/ldr_#{version}_darwin_arm64.tar.gz"
       sha256 "9f3bd6f93f0f9690ad7472d0bdf5bc3a98080ef8027b8840e2fcf988c02588ea"
     end
 
     on_intel do
-      url
-      "https://github.com/swsw1005/local-dev-launcher/releases/download/v#{version}/ldr_#{version}_darwin_amd64.tar.gz"
+      url "https://github.com/swsw1005/local-dev-launcher/releases/download/v#{version}/ldr_#{version}_darwin_amd64.tar.gz"
       sha256 "08da0b1f480aca252d82d3986c0b43b54c98dfda4c4b93b4eb2957833b41bd8f"
     end
   end
