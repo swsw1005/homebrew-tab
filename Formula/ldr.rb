28be78ac034c58ec03fd473d46eec1c92b878ac7
swsw1005 <59690177+swsw1005@users.noreply.github.com>
2026-09-14 11:45:13 +0900

    0.3.0

--- Formula/ldr.rb
+++ Formula/ldr.rb
@@ -6,12 +6,12 @@ class Ldr < Formula
   on_macos do
     on_arm do
       url "https://github.com/swsw1005/local-dev-launcher/releases/download/v#{version}/ldr_#{version}_darwin_arm64.tar.gz"
-      sha256 "e062b584e907a6af375a06c433c3aae3b5478040fd8a469acbf475cf42fc3a50"
+      sha256 "59801ae625cce46d34c80b542cb729848158c4dc7439eca1fda224224e76ed86"
     end
 
     on_intel do
       url "https://github.com/swsw1005/local-dev-launcher/releases/download/v#{version}/ldr_#{version}_darwin_amd64.tar.gz"
-      sha256 "f1f4d848d98931c7348e7345ba6b9df69b42536099245bfd4432799bd28be6e6" 
+      sha256 "d07aeff29ed78836e0955d7c22abae8341c02d7e3d7f3d52763550450d906efe" 
     end
   end
 
